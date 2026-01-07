1c5181901038ba849f6992f3b92a4a5caef8ca00
Ka Ho Wong <kwong49@uic.edu>
2026-01-07 14:50:05 -0600

    Update __init__.py

--- corral_framework/__init__.py
+++ corral_framework/__init__.py
@@ -6,16 +6,22 @@ and effective topological Hamiltonians for Magnetic-Superconductor Hybrid system
 confined in a quantum corral, with either quasi-1D or arbitrary 2D geometry.
 """
 
-from .MSH_Quasi_1D import Topo_Ham_1D
+from .MSH_Quasi_1D import GF_Hybrid, Topo_Ham_1D
 from .MSH_2D_Library import (
-    build_GF_library, build_GF_matrix_final, build_corral_basis, build_T_matrix, Topo_Ham_2D
+    build_GF_library, 
+    build_GF_matrix_final, 
+    build_corral_basis, 
+    build_T_matrix, 
+    Topo_Ham_2D
 )
 
 __version__ = "0.1.0"
 
 __all__ = [
+    "GF_Hybrid",
     "Topo_Ham_1D",
     "build_GF_library",
+    "apply_GF_library_vectorized",
     "build_GF_matrix_final",
     "build_corral_basis",
     "build_T_matrix",
